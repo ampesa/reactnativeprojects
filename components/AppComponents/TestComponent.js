c134461ce6e0fbae97ccd8be7d01f387bb9739b8
ampesa <penella.amadeo@gmail.com>
2019-03-18 23:03:51 +0100

    Firebase starter project, first commit

---
+++ components/AppComponents/TestComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+export default class TestComponent extends Component {
+
+    render(){
+        return(
+            <Text>TestComponent</Text>
+        )
+    }
+}
